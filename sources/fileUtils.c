644cf8c293e2250a4b372f25c37a26706363c18c
Perkinss27 <cyprien.gibault@efrei.net>
2022-10-17 15:54:04 +0200

    added file IO files

---
+++ sources/fileUtils.c
@@ -0,0 +1,10 @@
+//
+// Created by Cyprien on 17/10/2022.
+//
+
+#include "fileUtils.h"
+
+char** readFile(char* fpath){;
+    char** t;
+    return t;
+}
---
+++ sources/fileUtils.h
@@ -0,0 +1,13 @@
+//
+// Created by Cyprien on 17/10/2022.
+//
+
+#ifndef PROJET_C_FILEUTILS_H
+#define PROJET_C_FILEUTILS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char** readFile(char*);
+
+#endif //PROJET_C_FILEUTILS_H
