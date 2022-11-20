305bf4e8241934649941d3db44bf9de67eddc21a
HUngryGOrilla <loreal.hugo@gmail.com>
2022-11-20 16:28:32 +0100

    commentaries

--- sources/treeUtils.c
+++ sources/treeUtils.c
@@ -60,25 +60,26 @@ char* findwordoftype(p_root my_tree,int type){
     char* word;
     word=(char*) malloc(sizeof(char)*25);
     int n;
-    int nextexist=1,foundword=0;
-    while (foundword==0){
-        temp=my_tree->node->next[type];
+    int nextexist=1,foundword=0; //boolean to check the different cases
+    while (foundword==0){ //as long as we did not take a word
+        //if we start again we have to reset some variables
+        temp=subtree;
         int k=0;
         word="";
-        while(nextexist==1){
+        while(nextexist==1){ //as long as we still have possibilities for the next letter
             if (temp->derives!=NULL){
-                foundword=rand()%2;
+                foundword=rand()%2; //if this is the end of a word then 50% chance of taking it
             }
             if (foundword==0){
                 word[k]=temp->val;
                 n=0;
                 for (int i=0;i<26;i++){
-                    if(temp->next[i]!=NULL){
+                    if(temp->next[i]!=NULL){ // find the number of possible letters
                         n++;
                     }
                 }
                 if (n==0){
-                    nextexist=0;
+                    nextexist=0; //check if there are any
                 }
                 else{
                     int* tab=(int*) malloc(sizeof(int)*n);
@@ -86,16 +87,16 @@ char* findwordoftype(p_root my_tree,int type){
                     for (int i=0;i<26;i++){
                         if(temp->next[i]!=NULL){
                             tab[n]=i;
-                            n++;
+                            n++; //fill the tab of possible next letters
                         }
                     }
                     int nextl;
-                    nextl=rand()%n;
-                    temp=temp->next[nextl];
+                    nextl=rand()%n;//choose one
+                    temp=temp->next[nextl];//and go to the next letter
                 }
             }
             else{
-                nextexist=0;
+                nextexist=0; //if we have a word we need to get out of the loop
             }
             nextexist=0;//to delete
 
